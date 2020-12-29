6aee18a397f89b5d51e598452a6b7e5ec7438865
kuniaki <k.ishikawa.0128@gmail.com>
2020-12-29 12:59:30 +0900

    remove comment

--- src/components/home/home.page.vue
+++ src/components/home/home.page.vue
@@ -25,14 +25,12 @@ export default defineComponent({
     const notes = ref<Note[]>([])
     const getNotes = async () => {
       const result = await API.graphql(graphqlOperation(listNotes)) as { data: ListNotesQuery }
-      // TODO: Refactoring
       if (result.data.listNotes && result.data.listNotes.length > 0) {
         notes.value = result.data.listNotes?.map((note) => note as Note)
       }
     }
     const subscribeNote = async () => {
       const result = await API.graphql(graphqlOperation(onCreateNote)) as Observable<object>
-      // TODO: Refactoring
       result.subscribe({
         next: ({ value: { data } }: NoteSubscriptionEvent) => {
           notes.value.push(data.onCreateNote as Note)
