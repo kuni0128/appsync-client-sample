5ab290cfc667d558fbf8fe9fdf24f276ec13ba4e
kuniaki <k.ishikawa.0128@gmail.com>
2020-12-29 10:21:19 +0900

    setting amplify client

---
+++ src/aws-exports.ts
@@ -0,0 +1,7 @@
+﻿/* eslint-disable quote-props */
+export const appsyncConfig = {
+  'aws_appsync_graphqlEndpoint': 'https://z2tyi5f5yfb4xhlobiw3rnjd5e.appsync-api.us-east-1.amazonaws.com/graphql',
+  'aws_appsync_region': 'us-east-1',
+  'aws_appsync_authenticationType': 'API_KEY',
+  'aws_appsync_apiKey': 'da2-i2rnn2xxmjej5pvkxpon5kcwoe'
+}
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,8 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import Amplify from 'aws-amplify'
+import { appsyncConfig } from './aws-exports'
 
+Amplify.configure(appsyncConfig)
 createApp(App).use(store).use(router).mount('#app')
